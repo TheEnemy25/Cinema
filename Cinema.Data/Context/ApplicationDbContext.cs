605a4ec27a53eff83ad92b1e450b13db995926dd
TheEnemy25 <andrikkobrin@gmail.com>
2023-07-30 15:54:12 +0300

    Configured Identity and IdentityServer

--- Cinema.Data/Context/ApplicationDbContext.cs
+++ Cinema.Data/Context/ApplicationDbContext.cs
@@ -18,7 +18,7 @@ namespace Cinema.Data.Context
             builder.Entity<User>(entity => entity.ToTable(name: "Users"));
             builder.Entity<IdentityRole>(entity => entity.ToTable(name: "Roles"));
             builder.Entity<IdentityUserRole<string>>(entity => entity.ToTable(name: "UserRoles"));
-            builder.Entity<IdentityUserClaim<string>>(entity => entity.ToTable(name: "UserClaim"));
+            builder.Entity<IdentityUserClaim<string>>(entity => entity.ToTable(name: "UserClaim"));  
             builder.Entity<IdentityUserLogin<string>>(entity => entity.ToTable(name: "UserLogins"));
             builder.Entity<IdentityUserToken<string>>(entity => entity.ToTable(name: "UserTokens"));
             builder.Entity<IdentityRoleClaim<string>>(entity => entity.ToTable(name: "UserClaims"));

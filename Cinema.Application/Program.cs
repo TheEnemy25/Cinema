67e6fc8bcfa8fe03ae5598046ab4db5e8e1827d8
Bohdan Kulish <bogdankulish5@gmail.com>
2023-10-26 18:07:05 +0300

    Update packages, add test controller

--- Cinema.Application/Program.cs
+++ Cinema.Application/Program.cs
@@ -1,6 +1,7 @@
 using Cinema.Application;
 using Cinema.Data.Context;
 using Cinema.Data.Entities;
+using Exam.Data.Infrastructure;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +10,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Auth", Version = "v1" });
+});
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
@@ -21,9 +28,13 @@ builder.Services.AddIdentity<AppUser, IdentityRole>(config =>
     config.Password.RequireDigit = true;
     config.Password.RequireNonAlphanumeric = false;
     config.Password.RequireUppercase = false;
+    config.User.RequireUniqueEmail = true;
 })
     .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddDefaultTokenProviders();
+    .AddDefaultTokenProviders()
+    .AddSignInManager<SignInManager<AppUser>>();
+
+builder.Services.AddScoped<IBaseRepository<Actor>, BaseRepository<Actor>>();
 
 builder.Services.AddIdentityServer()
     .AddAspNetIdentity<AppUser>()

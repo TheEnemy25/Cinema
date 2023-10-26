c0af03a51aa74bb54833e40252c8b2d410f8b138
Bohdan Kulish <bogdankulish5@gmail.com>
2023-10-26 18:07:05 +0300

    Update packages, add test controller

---
+++ Cinema.Application/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using Cinema.Application.Models.Register;
+using Cinema.Application.Models.SignIn;
+using Cinema.Data.Entities;
+using Exam.Data.Infrastructure;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cinema.Application.Controllers
+{
+    [AllowAnonymous]
+    [Route("~/test")]
+    public class TestController : ControllerBase
+    {
+        public TestController()
+        {
+
+        }
+
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            var userManager = HttpContext.RequestServices.GetRequiredService<UserManager<AppUser>>();
+            return Ok("pong");
+        }
+    }       
+}

3a3adc8e01cc018f8d10741e87cc2f82a376193d
Bohdan Kulish <bogdankulish5@gmail.com>
2023-12-14 19:31:02 +0200

    Added startup

---
+++ Cinema.API/Startup.cs
@@ -0,0 +1,22 @@
+﻿namespace Cinema.API
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+        }
+    }
+}
